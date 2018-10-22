b2e946aa907e9c5cf29ce51f274c2ce8053c3e6c
Jacob Wylie <jacobwylie@innovation.hrs>
2018-10-22 13:05:18 +0200

    add templates to serve in the view layer

--- django_tut_2/boards/views.py
+++ django_tut_2/boards/views.py
@@ -2,8 +2,9 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Board
 
 # Create your views here.
 def home(request):
-	return HttpResponse("Hello World")
+	boards = Board.objects.all()
+	return render(request, 'home.html', {'boards': boards})
